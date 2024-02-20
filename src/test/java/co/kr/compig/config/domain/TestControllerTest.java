018e3104d85f6a1c1a275e5490b0698f4f7d3856
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-02-20 14:28:21 +0900

    git setup

---
+++ src/test/java/co/kr/compig/config/domain/TestControllerTest.java
@@ -0,0 +1,15 @@
+package co.kr.compig.config.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class TestControllerTest {
+
+    @Test
+    public void test() {
+
+    }
+}
