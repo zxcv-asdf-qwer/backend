1a8f9d6e56fd1619b83d71148ac45889199a5884
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-02-20 16:33:58 +0900

    git setup

---
+++ src/main/java/co/kr/compig/common/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package co.kr.compig.common.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+            httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+        http.cors(withDefaults());
+        http.csrf(withDefaults());
+        http.authorizeHttpRequests((authorize) -> authorize
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .requestMatchers("/actuator/**", "/docs/**", "/test/**").permitAll()// external
+                .anyRequest().authenticated()// internal
+        ).oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+
+        return http.build();
+    }
+
+}
