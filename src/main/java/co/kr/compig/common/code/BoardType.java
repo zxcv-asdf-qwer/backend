733ec82845e6611a8e35a84d198bbe56526819b0
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-02-23 17:58:51 +0900

    fix: boardType 목록 추가

--- src/main/java/co/kr/compig/common/code/BoardType.java
+++ src/main/java/co/kr/compig/common/code/BoardType.java
@@ -7,7 +7,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum BoardType implements BaseEnumCode<String> {
-  NOTICE("NOT", "desc.board.notice"); // 공지사항
+  NOTICE("NOT", "공지사항"), // 공지사항
+  FAQ("FAQ", "FAQ"), // FAQ
+  NEWS("NEW", "소식"), // 소식
+  INFO("INF", "간병정보"), // 간병정보
+  EDUCATION("EDU", "교육"), // 교육
+  EVENT("EVE", "이벤트"); // 이벤트
 
   private final String code;
   private final String desc;
