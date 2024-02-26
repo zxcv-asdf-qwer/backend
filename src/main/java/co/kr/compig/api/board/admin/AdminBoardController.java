bca809082e693d125dcba30eca80f2ce44bfd653
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-02-26 12:59:43 +0900

    fix: RequestBody 추가

--- src/main/java/co/kr/compig/api/board/admin/AdminBoardController.java
+++ src/main/java/co/kr/compig/api/board/admin/AdminBoardController.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,28 +30,32 @@ public class AdminBoardController {
   private final BoardService boardService;
 
   @PostMapping
-  public ResponseEntity<Response<?>> createBoard(@Valid BoardCreateRequest boardCreateRequest){
+  public ResponseEntity<Response<?>> createBoard(
+      @RequestBody @Valid BoardCreateRequest boardCreateRequest) {
     return ResponseEntity.ok().body(Response.<Map<String, Long>>builder()
         .data(Map.of("boardId", boardService.createBoard(boardCreateRequest)))
         .build());
   }
 
   @GetMapping
-  public ResponseEntity<Response<Page<BoardResponse>>> pageListBoard(@RequestBody @Valid BoardSearchRequest boardSearchRequest, Pageable pageable) {
+  public ResponseEntity<Response<Page<BoardResponse>>> pageListBoard(
+      @RequestBody @Valid BoardSearchRequest boardSearchRequest, Pageable pageable) {
     return ResponseEntity.ok().body(Response.<Page<BoardResponse>>builder()
         .data(boardService.pageListBoard(boardSearchRequest, pageable))
         .build());
   }
 
   @GetMapping("/{boardId}")
-  public ResponseEntity<Response<BoardResponse>> getBoard(@PathVariable(name = "boardId") Long boardId){
+  public ResponseEntity<Response<BoardResponse>> getBoard(
+      @PathVariable(name = "boardId") Long boardId) {
     return ResponseEntity.ok(Response.<BoardResponse>builder()
         .data(boardService.getBoard(boardId))
         .build());
   }
 
   @PutMapping("/{boardId}")
-  public ResponseEntity<Response<?>> updateBoard(@PathVariable(name = "boardId") Long boardId, @RequestBody @Valid BoardUpdateRequest boardUpdateRequest){
+  public ResponseEntity<Response<?>> updateBoard(@PathVariable(name = "boardId") Long boardId,
+      @RequestBody @Valid BoardUpdateRequest boardUpdateRequest) {
     return ResponseEntity.ok().body(Response.<Map<String, Long>>builder()
         .data(Map.of("boardId", boardService.updateBoard(boardId, boardUpdateRequest)))
         .build());
@@ -61,7 +64,7 @@ public class AdminBoardController {
   @DeleteMapping(path = "/{boardId}")
   public ResponseEntity<Response<?>> deleteBoard(@PathVariable(name = "boardId") Long boardId) {
     return ResponseEntity.ok().body(Response.<Map<String, Long>>builder()
-            .data(Map.of("boardId", boardService.deleteBoard(boardId)))
-            .build());
+        .data(Map.of("boardId", boardService.deleteBoard(boardId)))
+        .build());
   }
 }
