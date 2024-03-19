8e9f971d0280403920831d517cfbf0e40f1599c1
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-03-19 13:32:52 +0900

    feat: 환자 테이블 컬럼 추가

--- src/main/java/co/kr/compig/api/domain/patient/Patient.java
+++ src/main/java/co/kr/compig/api/domain/patient/Patient.java
@@ -1,8 +1,17 @@
 package co.kr.compig.api.domain.patient;
 
+import co.kr.compig.api.domain.code.GenderCode;
+import co.kr.compig.api.domain.code.IsYn;
+import co.kr.compig.api.domain.code.ToiletType;
+import co.kr.compig.api.domain.code.converter.ToiletTypeConverter;
 import co.kr.compig.api.domain.member.Member;
+import co.kr.compig.global.embedded.CreatedAndUpdated;
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
@@ -37,8 +46,73 @@ public class Patient {
 	@Column(name = "patient_id")
 	private Long id;
 
+	@Column(length = 50)
+	private String patientNm;    // 환자 이름
+
+	@Column(length = 1)
+	@Enumerated(EnumType.STRING)
+	private GenderCode gender;    // 환자 성별
+
+	@Column
+	private Integer patientAge; // 환자 나이
+
+	@Column
+	private Integer patientHeight; // 환자 신장
+
+	@Column
+	private Integer patientWeight; // 환자 몸무게
+
+	@Column(length = 50)
+	private String diseaseNm; // 질환명
+
+	@Column(length = 10)
+	@Convert(converter = ToiletTypeConverter.class)
+	private ToiletType selfToiletAvailability; // 대소변 해결 여부
+
+	@Column(length = 1)
+	private IsYn moveAvailability; // 거동 가능 여부
+
+	@Column(length = 10)
+	private IsYn mealAvailability; // 식사 가능 여부
+
+	@Column(length = 1)
+	@Enumerated(EnumType.STRING)
+	private GenderCode genderPreference; // 선호 성별
+
+	@Column(length = 1)
+	@Enumerated(EnumType.STRING)
+	private IsYn covid19Test; // 코로나 검사 필요 여부
+
+	@Column
+	private String requestedTerm; // 요청 사항
+
+	@Column
+	private String locationType; // 간병 장소 종류
+
+	@Column(length = 10)
+	private String addressCd; // 간병 장소 우편 번호
+
+	@Column(length = 200)
+	private String address1; // 간병 장소 주소
+
+	@Column(length = 200)
+	private String address2; // 간병 장소 상세 주소
+
+	/* =================================================================
+	* Domain mapping
+	================================================================= */
 	@Builder.Default
 	@JoinColumn(name = "member_id", nullable = false, foreignKey = @ForeignKey(name = "fk01_patient"))
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Member member = new Member(); // Member id
+	/* =================================================================
+	* Relation method
+	================================================================= */
+
+	/* =================================================================
+   * Default columns
+   ================================================================= */
+	@Embedded
+	@Builder.Default
+	private CreatedAndUpdated createdAndModified = new CreatedAndUpdated();
 }
