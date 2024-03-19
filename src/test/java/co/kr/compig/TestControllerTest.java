a64228e1bab1d60d32e87b958c3b8ff79d2db07c
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-03-19 17:56:06 +0900

    remove: spring-boot-starter-oauth2-client

--- src/test/java/co/kr/compig/TestControllerTest.java
+++ src/test/java/co/kr/compig/TestControllerTest.java
@@ -2,7 +2,6 @@ package co.kr.compig;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
@@ -10,7 +9,5 @@ public class TestControllerTest {
 
 	@MockBean
 	private JwtDecoder jwtDecoder;
-	@MockBean
-	private ClientRegistrationRepository clientRegistrationRepository;
 
 }
