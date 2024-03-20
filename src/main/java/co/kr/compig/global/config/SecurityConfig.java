323d82e1aed5ce84e7a3140ea6c9df246e17b33a
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-03-20 18:15:31 +0900

    fix: 웹 소셜 로그인

--- src/main/java/co/kr/compig/global/config/SecurityConfig.java
+++ src/main/java/co/kr/compig/global/config/SecurityConfig.java
@@ -39,8 +39,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		//    http.csrf(withDefaults());
-		//    http.cors(withDefaults());
+		// http.csrf(withDefaults());
+		// http.cors(withDefaults());
 		http.csrf(AbstractHttpConfigurer::disable);
 		http.authorizeHttpRequests(auth -> auth
 			.requestMatchers(new AntPathRequestMatcher("/pb/**"))
