ca30c8e7005023efb0a36b5b1903b3aa53f420a9
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-03-21 18:38:20 +0900

    fix: doSocialLogin 확인용 추가

--- src/main/java/co/kr/compig/api/presentation/social/SocialController.java
+++ src/main/java/co/kr/compig/api/presentation/social/SocialController.java
@@ -3,12 +3,16 @@ package co.kr.compig.api.presentation.social;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.kr.compig.api.application.social.SocialUserService;
+import co.kr.compig.api.domain.code.ApplicationType;
+import co.kr.compig.api.domain.code.MemberRegisterType;
 import co.kr.compig.api.presentation.member.request.LeaveRequest;
 import co.kr.compig.api.presentation.social.request.SocialLoginRequest;
 import co.kr.compig.api.presentation.social.response.SocialLoginResponse;
@@ -25,6 +29,20 @@ public class SocialController {
 	@PostMapping(path = "/login")
 	public ResponseEntity<SocialLoginResponse> doSocialLogin(@RequestBody SocialLoginRequest socialLoginRequest) {
 
+		return ResponseEntity.created(URI.create("/social/login"))
+			.body(socialUserService.doSocialLogin(socialLoginRequest));
+	}
+
+	@GetMapping(path = "/login")
+	public ResponseEntity<?> doSocialLogin(
+		@RequestParam(name = "applicationType", required = false) ApplicationType applicationType,
+		@RequestParam(name = "memberRegisterType", required = false) MemberRegisterType memberRegisterType,
+		@RequestParam(name = "code", required = false) String code,
+		@RequestParam(name = "id_token", required = false) String token
+	) {
+		SocialLoginRequest socialLoginRequest = SocialLoginRequest.builder()
+			.token(token)
+			.code(code).memberRegisterType(memberRegisterType).applicationType(applicationType).build();
 		return ResponseEntity.created(URI.create("/social/login"))
 			.body(socialUserService.doSocialLogin(socialLoginRequest));
 	}
