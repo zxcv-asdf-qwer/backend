c718f3cd3eba1723685090b5d48f046c7ab8dcf0
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-03-22 15:24:57 +0900

    feat: 서버 시간 변경

--- src/main/java/co/kr/compig/CompigBackApplication.java
+++ src/main/java/co/kr/compig/CompigBackApplication.java
@@ -1,24 +1,20 @@
 package co.kr.compig;
 
-import java.util.TimeZone;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import jakarta.annotation.PostConstruct;
-
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @EnableFeignClients
 @SpringBootApplication
 public class CompigBackApplication {
-	@PostConstruct
-	public void started() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-	}
+	// @PostConstruct
+	// public void started() {
+	// 	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompigBackApplication.class, args);
