b6349290ee68662eaf31e7e449166c1fad56b559
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-03-25 18:18:27 +0900

    fix: sms request

--- src/main/java/co/kr/compig/api/presentation/sms/SmsController.java
+++ src/main/java/co/kr/compig/api/presentation/sms/SmsController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.kr.compig.api.application.sms.SmsService;
@@ -24,14 +23,14 @@ public class SmsController {
 	@PostMapping(path = "/authentication/{receiverPhoneNumber}")
 	public ResponseEntity<Response<?>> sendAuthentication(
 		@PathVariable(name = "receiverPhoneNumber") String receiverPhoneNumber) {
-		smsService.sendSmsAuthentication(receiverPhoneNumber);
+		smsService.sendSmsAuthentication(String.valueOf(receiverPhoneNumber));
 		return ResponseEntity.ok().build();
 	}
 
-	@GetMapping(path = "/authentication/{receiverPhoneNumber}")
+	@GetMapping(path = "/authentication/{receiverPhoneNumber}/{authenticationNumber}")
 	public ResponseEntity<Response<?>> getAuthentication(
 		@PathVariable(name = "receiverPhoneNumber") String receiverPhoneNumber,
-			@RequestParam(name = "authenticationNumber") String authenticationNumber) {
+		@PathVariable(name = "authenticationNumber") String authenticationNumber) {
 		smsService.getAuthentication(receiverPhoneNumber, authenticationNumber);
 		return ResponseEntity.ok().build();
 	}
