47c53619b0d44923ad5dc1b033179e6cbd062d5f
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-04-01 17:55:01 +0900

    fix: hospital select page ModelAttribute -> RequestParam

--- src/main/java/co/kr/compig/api/presentation/hospital/UserHospitalController.java
+++ src/main/java/co/kr/compig/api/presentation/hospital/UserHospitalController.java
@@ -5,9 +5,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.kr.compig.api.application.hospital.HospitalService;
@@ -33,8 +33,8 @@ public class UserHospitalController {
 	@Operation(summary = "조회")
 	@GetMapping
 	public ResponseEntity<SliceResponse<HospitalResponse>> pageListHospital(
-		@ParameterObject @ModelAttribute @Valid HospitalSearchRequest hospitalSearchRequest,
-		@ParameterObject Pageable pageable) {
+		@ParameterObject @RequestParam @Valid HospitalSearchRequest hospitalSearchRequest,
+		@RequestParam Pageable pageable) {
 		Slice<HospitalResponse> slice = hospitalService.pageListHospitalCursor(hospitalSearchRequest, pageable);
 		SliceResponse<HospitalResponse> sliceResponse = new SliceResponse<>(slice.getContent(), pageable,
 			slice.hasNext());
