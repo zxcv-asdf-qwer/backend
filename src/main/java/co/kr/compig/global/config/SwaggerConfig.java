318b15a0e3bd439d8dabd91862a518244860137e
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-04-03 13:01:52 +0900

    fix: swagger cors

--- src/main/java/co/kr/compig/global/config/SwaggerConfig.java
+++ src/main/java/co/kr/compig/global/config/SwaggerConfig.java
@@ -1,17 +1,24 @@
 package co.kr.compig.global.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
 @OpenAPIDefinition(
-	info = @io.swagger.v3.oas.annotations.info.Info(title = "Compig API",
+	info = @Info(title = "Compig API",
 		description = "Compig API 명세",
 		version = "v1"))
-@io.swagger.v3.oas.annotations.security.SecurityScheme(
+@SecurityScheme(
 	name = "Bearer Authentication",
 	type = SecuritySchemeType.HTTP,
 	bearerFormat = "JWT",
@@ -19,6 +26,20 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 )
 @Configuration
 public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		List<Server> servers = new ArrayList<>();
+		Server localServer = new Server();
+		localServer.setUrl("http://localhost:8080");
+		servers.add(localServer);
+
+		Server devServer = new Server();
+		devServer.setUrl("https://api-dev.localhost.com");
+		servers.add(devServer);
+
+		return new OpenAPI()
+			.servers(List.of(localServer, devServer));
+	}
 
 	@Bean
 	public InternalResourceViewResolver defaultViewResolver() {
