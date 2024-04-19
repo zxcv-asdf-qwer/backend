fe2280d70ba79455dffad17923ec6cd9f3554f38
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-04-19 14:34:26 +0900

    fix: 신고 페이지 조회 수정

--- src/main/java/co/kr/compig/api/presentation/review/request/ReportSearchRequest.java
+++ src/main/java/co/kr/compig/api/presentation/review/request/ReportSearchRequest.java
@@ -3,10 +3,8 @@ package co.kr.compig.api.presentation.review.request;
 import co.kr.compig.global.dto.pagination.PageableRequest;
 import lombok.Data;
 import lombok.Getter;
-import lombok.experimental.SuperBuilder;
 
 @Getter
 @Data
-@SuperBuilder
 public class ReportSearchRequest extends PageableRequest {
 }
