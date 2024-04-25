4faeb4172daea4225e838d9aeaa180b2c16f98f5
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-04-25 14:46:38 +0900

    fix: 약관 TermsUpdateRequest 수정

--- src/main/java/co/kr/compig/api/presentation/board/request/TermsUpdateRequest.java
+++ src/main/java/co/kr/compig/api/presentation/board/request/TermsUpdateRequest.java
@@ -2,6 +2,7 @@ package co.kr.compig.api.presentation.board.request;
 
 import co.kr.compig.global.code.TermsType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TermsUpdateRequest {
+	@NotNull
 	@Schema(description = "약관 타입", example = "PERSONAL_INFORMATION_POLICY")
 	private TermsType termsType;
+	@NotNull
 	@Schema(description = "약관 내용", example = "약관 내용")
 	private String contents;
 }
