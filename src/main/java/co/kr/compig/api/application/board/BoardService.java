2790beb5d9e22d83da164eccd876e73a8c34d0ad
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-04-29 10:41:09 +0900

    fix: 게시판 생성 수정

--- src/main/java/co/kr/compig/api/application/board/BoardService.java
+++ src/main/java/co/kr/compig/api/application/board/BoardService.java
@@ -1,26 +1,20 @@
 package co.kr.compig.api.application.board;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import co.kr.compig.api.domain.board.Board;
 import co.kr.compig.api.domain.board.BoardRepository;
 import co.kr.compig.api.domain.board.BoardRepositoryCustom;
-import co.kr.compig.api.domain.file.SystemFile;
 import co.kr.compig.api.domain.file.SystemFileRepository;
 import co.kr.compig.api.presentation.board.request.BoardCreateRequest;
 import co.kr.compig.api.presentation.board.request.BoardSearchRequest;
 import co.kr.compig.api.presentation.board.request.BoardUpdateRequest;
 import co.kr.compig.api.presentation.board.response.BoardDetailResponse;
 import co.kr.compig.api.presentation.board.response.BoardResponse;
-import co.kr.compig.api.presentation.board.response.SystemFileResponse;
 import co.kr.compig.global.dto.pagination.SliceResponse;
 import co.kr.compig.global.error.exception.NotExistDataException;
 import co.kr.compig.global.utils.S3Util;
@@ -39,24 +33,11 @@ public class BoardService {
 	private final SystemFileRepository systemFileRepository;
 	private final S3Util s3Util;
 
-	public Long createBoard(BoardCreateRequest boardCreateRequest,
-		List<MultipartFile> files) {
-		Board board = new Board();
-		if (files != null) {
-			List<SystemFileResponse> imageUrlList = s3Util.uploadToFile(files);
-			board = boardCreateRequest.converterEntity();
-			boardRepository.save(board);
-			saveSystemFile(imageUrlList, board.getId());
-		} else {
-			board = boardCreateRequest.converterEntity();
-			boardRepository.save(board);
-		}
+	public Long createBoard(BoardCreateRequest boardCreateRequest) {
+		Board board = boardCreateRequest.converterEntity();
+		boardRepository.save(board);
 
 		return board.getId();
-	}
-
-	public String uploadImage(MultipartFile file) {
-		return s3Util.upload(file);
 	}
 
 	@Transactional(readOnly = true)
@@ -82,34 +63,36 @@ public class BoardService {
 		return board.toBoardDetailResponse();
 	}
 
-	public Long createBoardBaseFile(BoardCreateRequest boardCreateRequest,
-		Map<String, String> files) {
-		Board board = new Board();
-		if (files != null) {
-			List<SystemFileResponse> imageUrlList = s3Util.uploadBase64ToFile(files);
-			board = boardCreateRequest.converterEntity();
-			boardRepository.save(board);
-			saveSystemFile(imageUrlList, board.getId());
-		} else {
-			board = boardCreateRequest.converterEntity();
-			boardRepository.save(board);
-		}
+	//TODO 확인 후 삭제
 
-		return board.getId();
-	}
+	// public Long createBoardBaseFile(BoardCreateRequest boardCreateRequest,
+	// 	Map<String, String> files) {
+	// 	Board board = new Board();
+	// 	if (files != null) {
+	// 		List<SystemFileResponse> imageUrlList = s3Util.uploadBase64ToFile(files);
+	// 		board = boardCreateRequest.converterEntity();
+	// 		boardRepository.save(board);
+	// 		saveSystemFile(imageUrlList, board.getId());
+	// 	} else {
+	// 		board = boardCreateRequest.converterEntity();
+	// 		boardRepository.save(board);
+	// 	}
+	//
+	// 	return board.getId();
+	// }
 
-	private void saveSystemFile(List<SystemFileResponse> systemFileResponses, Long boardId) {
-		Board board = boardRepository.findById(boardId).orElseThrow(NotExistDataException::new);
-		for (SystemFileResponse systemFileResponse : systemFileResponses) {
-			SystemFile systemFile = SystemFile.builder()
-				.filePath(systemFileResponse.getFilePath())
-				.fileNm(systemFileResponse.getFileNm())
-				.fileExtension(systemFileResponse.getFileExtension())
-				.board(board)
-				.build();
-			systemFileRepository.save(systemFile);
-		}
-	}
+	// private void saveSystemFile(List<SystemFileResponse> systemFileResponses, Long boardId) {
+	// 	Board board = boardRepository.findById(boardId).orElseThrow(NotExistDataException::new);
+	// 	for (SystemFileResponse systemFileResponse : systemFileResponses) {
+	// 		SystemFile systemFile = SystemFile.builder()
+	// 			.filePath(systemFileResponse.getFilePath())
+	// 			.fileNm(systemFileResponse.getFileNm())
+	// 			.fileExtension(systemFileResponse.getFileExtension())
+	// 			.board(board)
+	// 			.build();
+	// 		systemFileRepository.save(systemFile);
+	// 	}
+	// }
 
 	@Transactional(readOnly = true)
 	public SliceResponse<BoardResponse> getBoardSlice(@Valid BoardSearchRequest request) {
--- src/main/java/co/kr/compig/api/presentation/board/AdminBoardController.java
+++ src/main/java/co/kr/compig/api/presentation/board/AdminBoardController.java
@@ -1,11 +1,9 @@
 package co.kr.compig.api.presentation.board;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,9 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import co.kr.compig.api.application.board.BoardService;
 import co.kr.compig.api.presentation.board.request.BoardCreateRequest;
@@ -44,21 +40,11 @@ public class AdminBoardController {
 	private final BoardService boardService;
 
 	@Operation(summary = "생성하기")
-	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+	@PostMapping
 	public ResponseEntity<Response<?>> createBoard(
-		@RequestPart(value = "boardCreateRequest") @Valid BoardCreateRequest boardCreateRequest,
-		@RequestPart(value = "file", required = false) List<MultipartFile> files) {
+		@RequestBody @Valid BoardCreateRequest boardCreateRequest) {
 		return ResponseEntity.ok().body(Response.<Map<String, Long>>builder()
-			.data(Map.of("boardId", boardService.createBoard(boardCreateRequest, files)))
-			.build());
-	}
-
-	@Operation(summary = "이미지 업로드")
-	@PostMapping(path = "/imageUpload")
-	public ResponseEntity<Response<?>> uploadImage(
-		@RequestPart(value = "file", required = false) MultipartFile file) {
-		return ResponseEntity.ok().body(Response.<String>builder()
-			.data(boardService.uploadImage(file))
+			.data(Map.of("boardId", boardService.createBoard(boardCreateRequest)))
 			.build());
 	}
 
@@ -98,30 +84,32 @@ public class AdminBoardController {
 		return ResponseEntity.ok().build();
 	}
 
-	/**
-	 * base64로 인코딩 된 파일 받아 게시물 생성
-	 */
-	@Operation(summary = "생성하기 - base64")
-	@PostMapping(path = "/base64")
-	public ResponseEntity<Response<?>> createBoardBase64(
-		@ParameterObject @Valid BoardCreateRequest boardCreateRequest,
-		@RequestPart(value = "file", required = false) Map<String, String> files
-	) {
-		return ResponseEntity.ok().body(Response.<Map<String, Long>>builder()
-			.data(Map.of("boardId", boardService.createBoardBaseFile(boardCreateRequest, files)))
-			.build());
-	}
+	//TODO 확인 후 삭제
 
-	/**
-	 * s3 파일 base64로 인코딩하여 보여주기
-	 */
-	@Operation(summary = "상세 조회 - base64")
-	@GetMapping(path = "/base64/{boardId}")
-	public ResponseEntity<Response<BoardDetailResponse>> getBoardBase(
-		@PathVariable(name = "boardId") Long boardId) {
-		return ResponseEntity.ok(Response.<BoardDetailResponse>builder()
-			.data(boardService.getBoard(boardId))
-			.build());
-	}
+	// /**
+	//  * base64로 인코딩 된 파일 받아 게시물 생성
+	//  */
+	// @Operation(summary = "생성하기 - base64")
+	// @PostMapping(path = "/base64")
+	// public ResponseEntity<Response<?>> createBoardBase64(
+	// 	@ParameterObject @Valid BoardCreateRequest boardCreateRequest,
+	// 	@RequestPart(value = "file", required = false) Map<String, String> files
+	// ) {
+	// 	return ResponseEntity.ok().body(Response.<Map<String, Long>>builder()
+	// 		.data(Map.of("boardId", boardService.createBoardBaseFile(boardCreateRequest, files)))
+	// 		.build());
+	// }
+	//
+	// /**
+	//  * s3 파일 base64로 인코딩하여 보여주기
+	//  */
+	// @Operation(summary = "상세 조회 - base64")
+	// @GetMapping(path = "/base64/{boardId}")
+	// public ResponseEntity<Response<BoardDetailResponse>> getBoardBase(
+	// 	@PathVariable(name = "boardId") Long boardId) {
+	// 	return ResponseEntity.ok(Response.<BoardDetailResponse>builder()
+	// 		.data(boardService.getBoard(boardId))
+	// 		.build());
+	// }
 
 }
