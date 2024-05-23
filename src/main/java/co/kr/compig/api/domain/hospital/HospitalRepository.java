1741dc643d9ebd00ad4ec49c01808956dd3348a1
zxcv-asdf-qwer <172124359+zxcv-asdf-qwer@users.noreply.github.com>
2024-05-23 15:53:31 +0900

    fix: hospital 삭제

--- src/main/java/co/kr/compig/api/domain/hospital/HospitalRepository.java
+++
@@ -1,9 +0,0 @@
-package co.kr.compig.api.domain.hospital;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface HospitalRepository extends JpaRepository<Hospital, Long>,
-	QuerydslPredicateExecutor<Hospital> {
-
-}
--- src/main/java/co/kr/compig/global/scheduler/CommonSchedulerTask.java
+++ src/main/java/co/kr/compig/global/scheduler/CommonSchedulerTask.java
@@ -2,7 +2,6 @@ package co.kr.compig.global.scheduler;
 
 import org.springframework.context.annotation.Profile;
 
-import co.kr.compig.api.application.hospital.HospitalSchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,11 +10,5 @@ import lombok.extern.slf4j.Slf4j;
 // @Component
 @RequiredArgsConstructor
 public class CommonSchedulerTask {
-	private final HospitalSchService hospitalSchService;
 
-	// @Scheduled(cron = "0 3 * * 1#1")
-	// @SchedulerLock(name = "HospitalService_insertHospitalInfo", lockAtLeastFor = "PT5M", lockAtMostFor = "PT10M")
-	// public void insertHospitalInfo() throws Exception {
-	// 	hospitalSchService.insertAllHospital();
-	// }
 }
